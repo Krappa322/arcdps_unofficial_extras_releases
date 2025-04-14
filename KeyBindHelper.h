0ff7f1a1379097d364b1a2b3c0988e18e2a5e735
knox <knoxfighter@asdff45.de>
2025-04-14 22:39:01 +0200

    add warning on missing option

--- KeyBindHelper.h
+++ KeyBindHelper.h
@@ -4,6 +4,10 @@
 
 #include <intsafe.h>
 
+#if __cplusplus == 199711L
+__pragma(message (__FILE__ "(" _CRT_STRINGIZE(__LINE__) "): warning: " "incompatible with C++99 or missing option /Zc:__cplusplus, https://learn.microsoft.com/en-us/cpp/build/reference/zc-cplusplus"))
+#endif
+
 #if __cplusplus >= 201703L
 #include <optional>
 #endif
